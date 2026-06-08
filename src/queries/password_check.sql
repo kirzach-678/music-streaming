f6de00774cd08cd11b0a23a04eb8e20d7507e41b
evtish <evtish@proton.me>
2026-06-08 13:38:58 +0300

    томатик смена пароля

--- src/queries/password_check.sql
+++ src/queries/password_check.sql
@@ -1,11 +1,5 @@
 -- проверка корректности хэша введённого пароля при авторизации
--- SELECT user_id,
--- 	name
--- FROM users
--- WHERE lower(email) = lower('pomidorka@soundteam.com')
--- 	AND password_hash = crypt('password', password_hash)
--- 	AND deleted = FALSE;
-SELECT (password_hash = crypt('правильный пароль', password_hash)) AS password_match
+SELECT (password_hash = crypt('fixiki2008', password_hash)) AS password_match
 FROM users
 WHERE lower(email) = 'pomidorka@soundstream.com';
 
